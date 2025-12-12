047bd447a749be9c41cfbeca532e333e9578b430
zap8600 <zapost86@gmail.com>
2025-12-12 12:34:30 -0600

    wip

--- sh.c
+++ sh.c
@@ -3,6 +3,28 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define BUFSIZE 1024
+
+char* read_line() {
+    size_t bufsize = BUFSIZE;
+    char* buf = (char*)malloc(bufsize * sizeof(char));
+    size_t pos = 0;
+    int c;
+    while(1) {
+        c = getchar();
+        if((c == EOF || c == '\n')) {
+            buf[pos] = '\0';
+            return pos;
+        } else {
+            buf[pos++] = (char)c;
+        }
+        if(pos >= bufsize) {
+            bufsize += BUFSIZE;
+            buf = (char*)realloc(buf, bufsize);
+        }
+    }
+}
+
 int main() {
     setbuf(stdout, NULL);
     setbuf(stderr, NULL);
